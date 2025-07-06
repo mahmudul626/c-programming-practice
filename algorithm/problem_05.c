3ef1879cfdd59f9f08f3719baca3477b9c7bd5a6
Md. Mahmudul Hasan Mabud <mdmahmudulhasan1511@gmail.com>
2025-07-06 21:15:43 +0600

    Update problem_05.c

--- algorithm/problem_05.c
+++ algorithm/problem_05.c
@@ -1,3 +1,31 @@
+/*
+Problem: Divisible Sum Pairs
+
+Given an array of integers and a positive integer k, find the number of pairs (i, j) such that:
+- i < j
+- (ar[i] + ar[j]) is divisible by k (i.e., (ar[i] + ar[j]) % k == 0)
+
+Input:
+- First line contains two integers n (size of array) and k (the divisor)
+- Second line contains n space-separated integers representing the array elements
+
+Output:
+- Print the number of valid pairs satisfying the above condition.
+
+Example:
+Input:
+6 3
+1 3 2 6 1 2
+
+Output:
+5
+
+Explanation:
+The valid pairs are:
+(0,2), (0,5), (1,3), (2,4), (4,5)
+where indices are 0-based.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
